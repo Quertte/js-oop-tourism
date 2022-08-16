13a7a5fa7dc84770fe9ae9e9f0ecddb946367170
Quertte <a.injen90@gmail.com>
2022-08-16 14:34:46 +0300

    all tests are passed

--- Agency.js
+++ Agency.js
@@ -1,18 +1,19 @@
 class Agency {
   constructor(money, tours = []) {
-
+    this.money = money;
+    this.tours = tours;
   }
 
   availableToursCount() {
-
+    return this.tours.length;
   }
 
   findTour(countryName) {
-
+    return this.tours.find(el => el.country === countryName);
   }
 
   sell(countryName) {
-
+    this.findTour(countryName);
   }
 }
 
--- Tourist.js
+++ Tourist.js
@@ -1,10 +1,18 @@
-class Tourist {
-  constructor() {
 
+class Tourist {
+  constructor(name, age, tours = []) {
+    this.name = name;
+    this.age = age;
+    this.tours = tours;
   }
 
   buy(countryName, agency) {
-
+    let tour = agency.tours.find(el => el.country === countryName);
+    if (tour) {
+      this.tours.push(tour);
+      return true;
+    }
+    return false;
   }
 }
 
